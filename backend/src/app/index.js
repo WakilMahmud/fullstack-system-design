fa481d19dbc695d186ca29f604874936c31b31f2
Wakil Mahmud <wakilmahmud30@gmail.com>
2025-12-20 21:02:37 +0600

    feat: add CORS support to backend application

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import cors from "cors";
 import globalErrorHandler from '../middlewares/globalErrorHandler.js';
 import notFound from '../middlewares/notFound.js';
 import router from '../routes/index.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // application routes
