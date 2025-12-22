a33b6623cddafae4aa99520f190eabdb65e1e672
Wakil Mahmud <wakil.mahmud@neural-semiconductor.com>
2025-12-22 22:36:27 +0600

    frontend: updated vite config for file watching issues in Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,        // equivalent to --host
+    watch: {
+      usePolling: true // fixes file watching issues in Docker
+    }
+  }
 })
