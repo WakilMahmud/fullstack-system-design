744194d605639cf0224d0f67ec0b60af8e46236e
Wakil Mahmud <wakil.mahmud@neural-semiconductor.com>
2025-12-25 15:58:18 +0600

    feat: add jsconfig.json for path aliasing and update imports in router.jsx to use alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const __dirname = path.resolve(path.dirname(''));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // Alias '@' to 'src' folder
+    },
+  },
   server: {
     host: true,        // equivalent to --host
     watch: {
